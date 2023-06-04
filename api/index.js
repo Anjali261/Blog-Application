5631583e35fb07a8f2304ffc219a530961eb91de
Anjali261 <mail2anjali.singh2610@gmail.com>
2023-06-04 11:07:54 +0530

    Backend setup

---
+++ api/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+var cors = require('cors');
+var cookieParser = require('cookie-parser');
+
+const errorHandler = require("./middleware/error")
+
+//MIDDLEWARE 
+app.use(morgan('dev'));
+app.use(bodyParser.json({limit: "5mb"}));
+app.use(bodyParser.urlencoded({
+    limit:"5mb",
+    extended:true
+}));
+app.use(cookieParser());
+app.use(cors());
+
+
+
+//import routes
+
+const authRoutes = require("./routes/authRoutes")
+
+//Routes Middleware
+app.use("/api",authRoutes );
+
+//Databse connectivity
+
+mongoose.set('strictQuery', false);
+
+const connectDB = async() =>{
+    try{
+        const con = await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser:true,
+
+        })
+        console.log(`MongoDB connected: ${con.connection.host}`);
+        
+    }catch(error){
+        console.log(error);
+        process.exit(1);
+
+    }
+}
+connectDB();
+
+
+
+
+
+//PORT
+
+app.listen(process.env.PORT , () =>{
+    console.log(`server is running at http://localhost:${process.env.PORT}`);
+
+});
